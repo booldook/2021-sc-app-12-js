f5e0d78daf319cf2a5e7b7a8a1299f49e65a86ea
booldook <booldook@gmail.com>
2021-08-26 17:10:00 +0900

    14.weather.js

--- js/14.weather.js
+++ js/14.weather.js
@@ -1,13 +1,11 @@
 // 02efdd64bdc14b279bc91d9247db4722
 // https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=02efdd64bdc14b279bc91d9247db4722&units=metric
+// http://openweathermap.org/img/wn/10d@2x.png
 
 // ? 문제 1. ../json/city.list.json의 모든 데이터를 cityList: Array에 담으시오.
-// 		($.get, axios, XMLHttpRequest 셋중의 하나를 선택하시오)
-
+// ? ($.get, axios, XMLHttpRequest 셋중의 하나를 선택하시오)
 // ? 문제 2 .가져온 데이터에서 한국의 정보만을 korCity: Array에 담으시오.
 // ? 문제 3. korCity에서 { 도시이름 city, 위도 lat, 경도 lon, id } 객체를 city: Array에 담으시오.
-
-// cityList | korCity | city
 
 /*
 axios.get('../json/city.list.json').then(onGet).catch(onError);
@@ -25,6 +23,52 @@ function onError(err) {
 	console.log(err);
 }*/
 
+// navigator.clipboard.writeText("<Hello World>").then(() => { console.log('카피되었습니다.') })
+
+navigator.geolocation.getCurrentPosition(onCoordSuccess, onCoordError);
+function onCoordSuccess(r) {
+	let { latitude: lat, longitude: lon } = r.coords;
+	console.log(lat, lon);
+}
+function onCoordError(err) {
+	// 1835848
+	console.log(err);
+}
 
 
-// http://openweathermap.org/img/wn/10d@2x.png
+
+
+
+/*************** global init **************/
+const appid = '02efdd64bdc14b279bc91d9247db4722';
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+const icons = ['http://openweathermap.org/img/wn/', '@2x.png'];
+const param = { units: 'metric', appid };
+
+/************** user function *************/
+function init() {
+	navigator.geolocation.getCurrentPosition(onCoordSuccess, onCoordError);
+	function onCoordSuccess(r) {
+		let { latitude: lat, longitude: lon } = r.coords;
+		axios.get(url, { params: { ...param, lat, lon } }).then(onGetWeather).catch(onError);
+	}
+	function onCoordError(err) {
+		axios.get(url, { params: { ...param, id: '1835848' } }).then(onGetWeather).catch(onError);
+	}
+}
+
+/************** event callback ************/
+function onGetWeather(r) {
+	console.log(r.data);
+}
+
+function onError(err) {
+	console.log(err);
+}
+
+/*************** event init ***************/
+
+
+/*************** start init ***************/
+init();
+
