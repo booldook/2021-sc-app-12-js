15c3825eb09a58d8a70da3c3c0f8726bcb1ade04
booldook <booldook@gmail.com>
2021-08-27 16:03:07 +0900

    14.weather

--- js/14.weather.js
+++ js/14.weather.js
@@ -56,7 +56,8 @@ function onCoordError(err) {
 
 /*************** global init **************/
 const appid = '02efdd64bdc14b279bc91d9247db4722';
-const url = 'https://api.openweathermap.org/data/2.5/weather';
+// const url = 'https://api.openweathermap.org/data/2.5/weather';
+const url = 'http://127.0.0.1:3000/weather';
 const icons = ['http://openweathermap.org/img/wn/', '@2x.png'];
 const param = { units: 'metric', appid };
 
